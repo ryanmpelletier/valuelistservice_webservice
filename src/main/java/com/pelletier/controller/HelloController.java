96753b390845084ec0eefd1643e2a7a7e7ac2e57
Pelletier <578993@bah.com>
2016-06-10 07:57:25 -0400

    Add request to method signature.

--- src/main/java/com/pelletier/controller/HelloController.java
+++ src/main/java/com/pelletier/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.pelletier.controller;
 
 import com.pelletier.valuelist.ValueListService;
+import com.pelletier.valuelist.Values;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by 578993 on 6/10/2016.
@@ -16,7 +19,7 @@ public class HelloController {
     ValueListService valueListService;
 
     @RequestMapping(value = "/valueslistservice/values", method = RequestMethod.GET)
-    public String hello(){
-        return "Hello";
+    public Values<? extends Object> getValueList(HttpServletRequest request){
+        return null;
     }
 }
