b68accd112ecee99feed5bcecc6d8016bd89d65a
Pelletier <578993@bah.com>
2016-06-10 08:01:52 -0400

    Add ValueListService code.

--- src/main/java/com/pelletier/controller/HelloController.java
+++ src/main/java/com/pelletier/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.pelletier.controller;
 
+import com.pelletier.valuelist.PagingInfo;
 import com.pelletier.valuelist.ValueListService;
 import com.pelletier.valuelist.Values;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Ryan Pelletier on 6/10/2016.
@@ -20,6 +23,23 @@ public class HelloController {
 
     @RequestMapping(value = "/valueslistservice/values", method = RequestMethod.GET)
     public Values<? extends Object> getValueList(HttpServletRequest request){
-        return null;
+        Map<String,Object> params = new HashMap<String, Object>();
+
+        for(Object key : request.getParameterMap().keySet()){
+            String paramValue = ((String[])request.getParameterMap().get(key))[0];
+//            params.put((String)key,parameterConversionService.convertIfNeeded((String) key, paramValue));
+            params.put((String)key,paramValue);
+
+        }
+        PagingInfo pagingInfo = null;
+
+        if(params.get("page") != null && params.get("numberPerPage") != null){
+            pagingInfo = new PagingInfo();
+            pagingInfo.setPage(Integer.parseInt((String)params.get("page")));
+            pagingInfo.setNumberPerPage(Integer.parseInt((String)params.get("numberPerPage")));
+        }
+
+        //the valuesInfo I am returned should never be null
+        return valueListService.getValuesList((String) params.get("valueListQuery"), params, pagingInfo);
     }
 }
