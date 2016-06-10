e4808d493380d21bcccffba1bfcf7e5b30a9e90a
Pelletier <578993@bah.com>
2016-06-10 07:54:55 -0400

    Autowire ValueListService

--- src/main/java/com/pelletier/controller/HelloController.java
+++ src/main/java/com/pelletier/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.pelletier.controller;
 
+import com.pelletier.valuelist.ValueListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+
+    @Autowired
+    ValueListService valueListService;
 
     @RequestMapping(value = "/valueslistservice/values", method = RequestMethod.GET)
     public String hello(){
