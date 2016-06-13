ab313c0c761bdecde02d430a7e15e2578be1c99d
Pelletier <578993@bah.com>
2016-06-13 12:21:16 -0400

    Add access control origin for local angular app.

--- src/main/java/com/pelletier/controller/ValueListServiceController.java
+++ src/main/java/com/pelletier/controller/ValueListServiceController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,7 +23,8 @@ public class ValueListServiceController {
     ValueListService valueListService;
 
     @RequestMapping(value = "/valueslistservice/values", method = RequestMethod.GET)
-    public Values<? extends Object> getValueList(HttpServletRequest request){
+    public Values<? extends Object> getValueList(HttpServletRequest request, HttpServletResponse response){
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:63342");
         Map<String,Object> params = new HashMap<String, Object>();
 
         for(Object key : request.getParameterMap().keySet()){
